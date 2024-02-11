43c5f51d7aea3d7060b58cf02ac37a2e5c1a4618
Ömrüm Çetin <omrum.cetin@magicmedia.studio>
2024-02-11 19:46:21 +0100

    Dot product and angle calculations added to vector

--- Animator/vector.h
+++ Animator/vector.h
@@ -13,7 +13,7 @@ struct FVector
         float V[3];
     };
     // default constructor
-    inline FVector()
+    FVector()
         : X(.0f)
         , Y(.0f)
         , Z(.0f)
@@ -33,7 +33,10 @@ struct FVector
         ,Y(InFV[1])
         ,Z(InFV[2]){}
 
-    float Dot(const FVector& InFirstVector, const FVector& InSecondVector);
+    float LenSq() const;
+    float Len() const;
+    void Normalize();
+    FVector Normalized() const;
 };
 
 inline FVector& operator+=(FVector& LeftOperand, const FVector& RightOperand);
@@ -41,3 +44,7 @@ inline FVector operator+(const FVector& LeftOperand, const FVector& RightOperand
 inline FVector operator-=(FVector& LeftOperand, const FVector& RightOperand);
 inline FVector operator-(const FVector& LeftOperand, const FVector& RightOperand);
 inline FVector operator*(const FVector& LeftOperand, const double& RightOperand);
+
+// Return in Radians
+float Angle(const FVector& v1, const FVector& v2);
+float Dot(const FVector& InFirstVector, const FVector& InSecondVector);
